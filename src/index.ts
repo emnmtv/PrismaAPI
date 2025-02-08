23bcfc474a04151d2aa586d46e1a960344db15de
emnmtv <90402959+emnmtv@users.noreply.github.com>
2025-02-09 01:22:25 +0800

    cors and port update
    1

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,20 @@ import express from 'express';
 import { authRouter } from './routes/authRoutes';
 import cors from "cors";
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3200;
 
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:3001", // Allow only requests from your frontend
+    origin: "*", // Allow requests from any origin
     methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
     allowedHeaders: "Content-Type,Authorization", // Allowed headers
     credentials: true, // Allow cookies and authorization headers
   })
 );
+
+
+
 app.use(express.json());
 app.use('/auth', authRouter);
 
